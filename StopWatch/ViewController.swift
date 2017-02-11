152ac491058117d4e54034e02b362c7a0ac9dd0e
Shigeo Sakamoto <shigeo.sakamoto@gmail.com>
2017-02-11 16:32:53 +0900

    計測結果をテーブルビューに保存するようにした

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     let buttonTextStart = "Start"
     let buttonTextStop = "Stop"
@@ -16,19 +16,13 @@ class ViewController: UIViewController {
     var min: Int = 0
     var sec: Int = 0
     var misec: Int = 0
+    var resultList = [String]()
     
     @IBOutlet weak var display: UILabel!
     @IBOutlet weak var startButton: UIButton!
-
-    @IBAction func resetButton(_ sender: Any) {
-        min = 0
-        sec = 0
-        misec = 0
-
-        updateDisplay()
-    }
-
-    @IBAction func Start(_ sender: Any) {
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBAction func start(_ sender: Any) {
         switch startButton.titleLabel?.text {
         case .some(buttonTextStart):
             timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
@@ -38,10 +32,21 @@ class ViewController: UIViewController {
         case .some(buttonTextStop):
             timer.invalidate()
 
+            resultList.insert(display.text!, at: 0)
+            tableView.reloadData()
+
             startButton.setTitle(buttonTextStart, for: .normal)
         default: break
         }
+    }
 
+
+    @IBAction func reset(_ sender: Any) {
+        min = 0
+        sec = 0
+        misec = 0
+
+        updateDisplay()
     }
 
     override func viewDidLoad() {
@@ -50,6 +55,9 @@ class ViewController: UIViewController {
         updateDisplay()
         startButton.setTitle(buttonTextStart, for: .normal)
         display.font = UIFont.monospacedDigitSystemFont(ofSize: 20, weight: 0.1)
+
+        tableView.estimatedRowHeight = 20
+        tableView.rowHeight = UITableViewAutomaticDimension
     }
 
     override func didReceiveMemoryWarning() {
@@ -79,6 +87,28 @@ class ViewController: UIViewController {
 
     func updateDisplay() {
         display.text = String(format: "%02d:%02d:%02d", min, sec, misec)
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return resultList.count
+    }
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "結果"
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell1", for: indexPath)
+
+        cell.textLabel!.text = resultList[indexPath.row]
+        cell.textLabel!.font = UIFont.monospacedDigitSystemFont(ofSize: 16, weight: 0.1)
+        
+        return cell
+
     }
 }
 
