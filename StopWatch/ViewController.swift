898323388c59896129d55b173fa15275044b7775
Shigeo Sakamoto <shigeo.sakamoto@gmail.com>
2017-02-11 11:48:59 +0900

    Timerの実装

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -12,6 +12,9 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
 
+    var timer: Timer!
+    var second: Int = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +24,23 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
+        timer.fire()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+
+        timer.invalidate()
+    }
+
+    func update() {
+        second += 1
+        display.text = "00:00:" + String(second)
     }
 }
 
